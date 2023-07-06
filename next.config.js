a58f794bf5477d73c45982cd82df308882b244bd
arun-mericaninc <arun@mericaninc.com>
2023-07-06 17:50:36 +0530

    deploy test3

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: false,
+  swcMinify: true,
+  images: {
+    domains: [],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
